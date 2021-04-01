1bd98b1e4c15f86d2326ecfe3251f8bf5f165a64
andvar95 <varleo.95@gmail.com>
2021-04-01 17:38:26 -0500

    nav_transparente

--- src/app/Components/navbar/navbar2/navbar2.component.ts
+++ src/app/Components/navbar/navbar2/navbar2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar2',
@@ -12,4 +12,24 @@ export class Navbar2Component implements OnInit {
   ngOnInit(): void {
   }
 
+  @HostListener("scroll", ['$event'])
+  doSomethingOnInternalScroll(event:any){
+    let scrollOffset = event.srcElement.scrollingElement.scrollTop;
+    console.log("scroll: ", scrollOffset);
+
+    if(scrollOffset>30){
+      document.querySelector('#nav')?.classList.add('nav_color')
+      document.querySelector('#nav')?.classList.remove('nav_transp')
+      document.querySelector("submenu")?.classList.remove("nav-sub")
+    }
+    else if (scrollOffset<30){
+
+      document.querySelector('#nav')?.classList.remove('nav_color')
+      document.querySelector('#nav')?.classList.add('nav_transp')
+      document.querySelector("submenu")?.classList.add("nav-sub")
+    }
+
+  }
 }
+
+
